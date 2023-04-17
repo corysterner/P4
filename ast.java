557999b3240ead9725f5c0289b12db720c2fb5c2
corysterner <corysterner@gmail.com>
2023-04-17 00:30:46 -0500

    Add files via upload

--- ast.java
+++ ast.java
@@ -1045,7 +1045,7 @@ class DotAccessExpNode extends ExpNode {
 			
 			//Ensure the RHS ID is declared in the record def
 			SymTab recordTable = lhsId.getRecordSymTab();
-			System.out.println(lhs.getName() + " " + lhs.getType());
+			System.out.println(lhsId.getName() + " " + lhsId.getType());
 //			if (recordTable == null) {
 //				Sym S = lhsId.getSym();
 //				recordTable = S.getTable();
